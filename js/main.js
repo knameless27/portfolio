1d65a00fec991ea3528a7ef555be31c63521544d
kuro nameless <knameless27@gmail.com>
2023-09-29 15:22:35 -0400

    feat: new content to the page

--- js/main.js
+++ js/main.js
@@ -110,3 +110,36 @@ const particles = {
 };
 
 particlesJS(particles);
+
+const leftButton = document.getElementById("leftBtn");
+const rightButton = document.getElementById("rightBtn");
+
+let actualContent = "home";
+
+const contents = ["home", "projects", "contact"];
+
+function showContent(event) {
+  let indexContent = contents.findIndex((content) => content == actualContent);
+  if (indexContent == -1) indexContent = 0;
+  let indexNewElement = getIndex(contents.length, indexContent, event);
+
+  // Oculta el contenido actual
+  document.getElementsByClassName(contents[indexContent])[0].style.display =
+    "none";
+
+  // Muestra el nuevo contenido
+  document.getElementsByClassName(contents[indexNewElement])[0].style.display =
+    "";
+
+  actualContent = contents[indexNewElement];
+}
+
+function getIndex(arrayLength, index, event) {
+  if (index == 0 && event == -1) return arrayLength - 1;
+  if (index == arrayLength - 1 && event == 1) return 0;
+  if (arrayLength > index + event) return index + event;
+  return 0;
+}
+
+leftButton.addEventListener("click", () => showContent(-1));
+rightButton.addEventListener("click", () => showContent(1));
