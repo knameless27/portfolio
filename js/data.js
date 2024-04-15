5ec759085905c0315f24af958578f72b5f00e33d
knameless27 <knameless27@gmail.com>
2024-04-15 12:10:54 -0400

    fix: responsive design and better skills

--- js/data.js
+++ js/data.js
@@ -14,39 +14,10 @@ const projects = [
     url: "https://github.com/knameless27/cinemaFront",
   },
   {
-    name: "Cinema back",
-    image: "https://i.postimg.cc/QKLcXcGp/cinema.png",
-    description:
-      "A functional cinema software (Backend)",
-    url: "https://github.com/knameless27/cinemaBack",
-  },
-  {
-    name: "Adolin",
-    image: "https://i.postimg.cc/Dz7zNp10/sw-ord.jpg",
-    description: "A functional software for a library (Frontend)",
-    url: "https://github.com/knameless27/Adolin",
-  },
-  {
     name: "Shallan",
     image: "https://i.postimg.cc/Dz7zNp10/sw-ord.jpg",
     description: "A functional software for a library (Backend)",
     url: "https://github.com/knameless27/Shallan",
-  },
-  {
-    name: "Weather",
-    image:
-      "https://static.vecteezy.com/system/resources/previews/009/344/657/original/sun-transparent-background-free-png.png",
-    description:
-      "Project made with react to verify the weather in your zone.",
-    url: "https://github.com/knameless27/weather",
-  },
-  {
-    name: "for28",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Escudo_de_la_Universidad_Surcolombiana.svg/1200px-Escudo_de_la_Universidad_Surcolombiana.svg.png",
-    description:
-      "project to improve the productivity in the university of colombia, usco.",
-    url: "https://knameless27.github.io/For28",
   },
 ];
 
